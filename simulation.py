983ba3cc8fbbdbd8ff795df676f3e0ca7463a4f8
Annie Cathignol <annie.cathignol@insa-lyon.fr>
2017-02-15 11:08:06 +0100

    Creation of the Simulation Class

---
+++ simulation.py
@@ -0,0 +1,16 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import population as pop
+
+
+
+class Simulation:
+  def __init__(self, lim_fitness, text_time, max_time):
+    self.lim_fitness = lim_fitness
+    self.text_time = text_time
+    self.max_time = max_time 
+    
+#P1 = pop.Population(5,0.5,2,0.1,0.1,0.2,[2])
+
