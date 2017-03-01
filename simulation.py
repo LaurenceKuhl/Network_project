8a29c0766d735fbfbadc78df8198543bca80590a
Annie Cathignol <annie.cathignol@insa-lyon.fr>
2017-03-01 11:10:38 +0100

    preparing the code simulation to work forever ;)

--- simulation.py
+++ simulation.py
@@ -29,12 +29,12 @@ class Simulation:
     #initialise the population
     pop1 = pop.Population(nb_nodes, p_edges, nb_graphs, preprod, pmut, qreprod, coeff)
     #run the simulation
-    for x in range(max_time):                          # OR in a second part “while True:”
+    for x in range(max_time):                  # OR in a second part “while True:”
       if x%text_time == 0 :
         save_pop()
 
       liste_f = pop1.fitness_list() 
-      if max(liste_f)==1 :
+      if max(liste_f) >= lim_fitness :
         save_pop()
         show_best() 
         return "Found it!"
@@ -49,3 +49,31 @@ class Simulation:
 
 P1 = pop.Population(5,0.5,2,0.1,0.1,0.2,[2])
 P1.mutation(2)
+
+#~ lim_fitness = argv[1]
+#~ text_time = argv[2]
+#~ max_time = argv[3]
+#~ 
+#~ nb_nodes = argv[4]
+#~ p_edges = argv[5]
+#~ nb_graphs = argv[6]
+#~ prob_reproduction = argv[7]
+#~ prob_mutation = argv[8]
+#~ qtty_reproduction = argv[9]
+#~ coefficients = argv[10]
+
+lim_fitness = 1
+text_time = 100
+max_time = 50
+
+nb_nodes = 5
+p_edges = 0.5
+nb_graphs = 2
+prob_reproduction = 0.1
+prob_mutation = 0.1
+qtty_reproduction = 0.2
+coefficients = [1,1,1]
+
+S = Simulation(lim_fitness, text_time, max_time, nb_nodes, p_edges, nb_graphs, prob_reproduction, prob_mutation, qtty_reproduction, coefficients)
+S.run()
+
