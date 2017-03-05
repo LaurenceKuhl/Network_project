1f4df1374dd1687c1b38f18ce417f016ad3daf9f
WaitndSee <antoine.weber@insa-lyon.fr>
2017-03-05 19:23:03 +0100

    Ajout methodes + debugging
    Ajout des méthodes save_pop() , load_pop() , show_best() 
    Tentative de correction de l'indentation (désolay)
    Divers mini correction dans le code pour faire tourner mes méthodes puis test rapide sur mini exemples
    Warning : version non définitive et pas encore compatible avec simulation.py

--- population.py
+++ population.py
@@ -6,135 +6,165 @@ import matplotlib.pyplot as plt
 import operator
 import copy
 import networkx as nx
+import os
 
 
 
 class Population:
-  def __init__(self, nb_nodes, p_edges, nb_graphs, prob_reproduction, prob_mutation, qtty_reproduction, coefficients):
-    self.nb_nodes = nb_nodes
-    self.p_edges = p_edges
-    self.nb_graphs = nb_graphs 
-    self.preprod = prob_reproduction
-    self.pmut = prob_mutation
-    self.qreprod = qtty_reproduction
-    self.coeff = coefficients
-    
-    self.graphs = []
-    for i in xrange(nb_graphs):
-      self.graphs.append(nx.fast_gnp_random_graph(self.nb_nodes, self.p_edges, seed=None, directed=False))
-      #print self.graphs[i].edges()
+    def __init__(self, nb_nodes, p_edges, nb_graphs, prob_reproduction, prob_mutation, qtty_reproduction, coefficients):
+        self.nb_nodes = nb_nodes
+        self.p_edges = p_edges
+        self.nb_graphs = nb_graphs 
+        self.preprod = prob_reproduction
+        self.pmut = prob_mutation
+        self.qreprod = qtty_reproduction
+        self.coeff = coefficients
+        self.graphs = []
+        for i in xrange(nb_graphs):
+            self.graphs.append(nx.fast_gnp_random_graph(self.nb_nodes, self.p_edges, seed=None, directed=False))
+            #print self.graphs[i].edges()
         
-  #========================= methods for the graph not predefined before
+    #========================= methods for the graph not predefined before
+      
+    def clust_powlaw(self, G):
+        # Checks if degree distribution follows power law distribution
+        # Returns value of gamma for graph G
+        gamma = []
+        fit = Fit(sorted(G.degree().values()))
+        return fit.power_law.alpha
   
-  def clust_powlaw(self, G):
-    # Checks if degree distribution follows power law distribution
-    # Returns value of gamma for graph G
-    gamma = []
-    fit = Fit(sorted(G.degree().values()))
-    return fit.power_law.alpha
-  
-  def clust_coef(self,G):
-    # Returns value of average clustering coefficient of each degree
-    avgcoef = []
-    degrees = []
-    tuplist = sorted(G.degree().items(), key=operator.itemgetter(1))
-    deg = -1
-    i = -1
-    L = []
-    X = []
-    for tup in tuplist:
-      if tup[1] > deg:
-        deg = tup[1]
-        i += 1
-        L.append([tup[0]])
-        degrees.append(tup[1])
-      elif tup[1] == deg:
-        L[i].append(tup[0])
-    for nodelist in L:
-        avgcoef.append(nx.average_clustering(G,nodelist))
-    return degrees, avgcoef
+    def clust_coef(self,G):
+        # Returns value of average clustering coefficient of each degree
+        avgcoef = []
+        degrees = []
+        tuplist = sorted(G.degree().items(), key=operator.itemgetter(1))
+        deg = -1
+        i = -1
+        L = []
+        X = []
+        for tup in tuplist:
+            if tup[1] > deg:
+                deg = tup[1]
+                i += 1
+                L.append([tup[0]])
+                degrees.append(tup[1])
+            elif tup[1] == deg:
+                L[i].append(tup[0])
+        for nodelist in L:
+            avgcoef.append(nx.average_clustering(G,nodelist))
+        return degrees, avgcoef
 
-  def fitness(self, G):# (returns a float between 0 and 1)	
-   # k is the sorted list of degrees, g is the gamma of the power law, 
-   # d is the diameter, c_k is the list of clustering coefficients
-    k, c_k = self.clust_coef(G)
-    g = self.clust_powlaw(G)
-    d = 0
-    list_d1 = []
-    for i in xrange (len(k)) :
-      list_d1.append( (c_k[i] - 1./k[i])**2 )
-    d1  =  np.mean (list_d1)# clustering fitness
-    if  g  >  2  and g  <  3 : # power law fitness
-      d2  =  0
-    else : d2  =  (g - 2.5)**2
-    d3  =  (d  -  math.log(math.log(self.nb_nodes)))**2
-    fit  =  1./(1 + self.coeff[0]*d1 + self.coeff[1]*d2 + self.coeff[2]*d3) #fitness
-    return fit
+    def fitness(self, G):# (returns a float between 0 and 1)	
+        # k is the sorted list of degrees, g is the gamma of the power law, 
+        # d is the diameter, c_k is the list of clustering coefficients
+        k, c_k = self.clust_coef(G)
+        g = self.clust_powlaw(G)
+        d = 0
+        list_d1 = []
+        for i in xrange (len(k)) :
+	    if not k[i] == 0 : # No idea what to do if k[i] equals 0 so i dont consider them here (Antoine)
+		list_d1.append( (c_k[i] - 1./k[i])**2 )
+        d1  =  np.mean (list_d1)# clustering fitness
+        if  g  >  2  and g  <  3 : # power law fitness
+	    d2  =  0
+        else : d2  =  (g - 2.5)**2
+        d3  =  (d  -  math.log(math.log(self.nb_nodes)))**2
+        fit  =  1./(1 + self.coeff[0]*d1 + self.coeff[1]*d2 + self.coeff[2]*d3) #fitness
+        return fit
 
 
   #========================================== methods for the population
   
-  def fitness_list(self):  #(returns a list of float)
-    list_f = []
-    for G in self.graphs:
-      liste_f.append(fitness(G))
-    return liste_f    
+    def fitness_list(self):  #(returns a list of float)
+	list_f = []
+	for G in self.graphs:
+	    list_f.append(self.fitness(G))
+	    print self.fitness(G)
+	return list_f    
 
-  def mutation(self , nb_newborns) : 	  # randomly mutate all the newborns
-	if nb_newborns  !=  0 :
-		c  =  0 # counter
-		while c  !=  nb_newborns :  # randomly delete one edge, then add a new one and check wether or not it was already there
-			m = random.uniform(0,1)
-			if m <= self.pmut :
-			  	deleted  =  random.randint(0, len( self.graphs[- c -1].edges() ) - 1 ) # random edge to be deleted 
-			  	self.graphs[- c-1].remove_edge(self.graphs[- c -1].edges()[deleted][0],self.graphs[- c-1].edges()[deleted][1]) # pop the edge
-			  	flag  =  False
-			  	while flag  ==  False :
-				  	node1  =  random.randint (0, len(self.graphs[-c-1].nodes()))
-				  	node2  =  random.randint (0,len (self.graphs[-c-1].nodes()))
-				  	if node1  !=  node2 and (min(node1,node2), max(node1,node2)) not in self.graphs[- c-1].edges() : # check if edge doesn't already exists
-					  	flag  =  True
-					  	self.graphs[-c-1].add_edge(min(node1,node2), max(node1,node2)) #new edge		
-					  	#what about left-without-edges nodes ?	
-			  	c += 1
+    def mutation(self , nb_newborns) : 	  # randomly mutate all the newborns
+        if nb_newborns  !=  0 :
+            c  =  0 # counter
+            while c  !=  nb_newborns :  # randomly delete one edge, then add a new one and check wether or not it was already there
+                m = random.uniform(0,1)
+                if m <= self.pmut :
+                    deleted  =  random.randint(0, len( self.graphs[- c -1].edges() ) - 1 ) # random edge to be deleted 
+                    self.graphs[- c-1].remove_edge(self.graphs[- c -1].edges()[deleted][0],self.graphs[- c-1].edges()[deleted][1]) # pop the edge
+                    flag  =  False
+                    while flag  ==  False :
+                        node1  =  random.randint (0, len(self.graphs[-c-1].nodes()))
+                        node2  =  random.randint (0,len (self.graphs[-c-1].nodes()))
+                        if node1  !=  node2 and (min(node1,node2), max(node1,node2)) not in self.graphs[- c-1].edges() : # check if edge doesn't already exists
+                            flag  =  True
+                            self.graphs[-c-1].add_edge(min(node1,node2), max(node1,node2)) #new edge		
+                            #what about left-without-edges nodes ?	
+                    c += 1
 
-  def reproduction(self):
-    list_sorted = self.fitness_list()
-    list_fit = copy.copy(list_fittest)
-    list_sorted.sort(reverse = True)
-    reprod_graphs = []
-    
-    for i in xrange(0, (self.qreprod)*(self.nb_graphs)):
-      reprod_graphs.append(self.graphs[list_fit.index(list_sorted[i])]) #So here I have the fittest graphs for the reproduction
-      
-    nbnewborn = 0
-    length_reprod = len(reprod_graphs)/2
-    
-    for i in xrange(0, length_reprod ,1):
-      individuals = random.sample(reprod_graphs, 2)
-      self.reprod_graphs.remove(individuals[0])
-      self.reprod_graphs.remove(individuals[1])
-      
-      if (random.uniform(0,1) < self.preprod) :
-        qty = int(random.uniform(0,1))
-        indiv1 = individuals[0].edges()
-        indiv2 = individuals[1].edges()
-        newborn = nx.fast_gnp_random_graph(self.nb_nodes, 0, seed=None, directed=False)
+    def reproduction(self):
+        list_sorted = self.fitness_list()
+        list_fit = copy.copy(list_fittest)
+        list_sorted.sort(reverse = True)
+        reprod_graphs = []
         
-        for i in xrange(0, qty*len(indiv1)):
-          newborn.add_edge(indiv1[i][0], indiv1[i][1])
+        for i in xrange(0, (self.qreprod)*(self.nb_graphs)):
+            reprod_graphs.append(self.graphs[list_fit.index(list_sorted[i])]) #So here I have the fittest graphs for the reproduction
+            nbnewborn = 0
+            length_reprod = len(reprod_graphs)/2
         
-        for i in xrange(0, (1-qty)*len(indiv2)):
-          newborn.add_edge(indiv2[i][0], indiv2[i][1])
+        for i in xrange(0, length_reprod ,1):
+            individuals = random.sample(reprod_graphs, 2)
+            self.reprod_graphs.remove(individuals[0])
+            self.reprod_graphs.remove(individuals[1])
+          
+            if (random.uniform(0,1) < self.preprod) :
+                qty = int(random.uniform(0,1))
+                indiv1 = individuals[0].edges()
+                indiv2 = individuals[1].edges()
+                newborn = nx.fast_gnp_random_graph(self.nb_nodes, 0, seed=None, directed=False)
+            
+            for i in xrange(0, qty*len(indiv1)):
+                newborn.add_edge(indiv1[i][0], indiv1[i][1])
+            
+            for i in xrange(0, (1-qty)*len(indiv2)):
+                newborn.add_edge(indiv2[i][0], indiv2[i][1])
+            
+            self.graphs.remove(self.graphs[list_fit.index(list_sorted[-1])])
+            self.graphs.append(newborn)
+            nbnewborn+=1
         
-        self.graphs.remove(self.graphs[list_fit.index(list_sorted[-1])])
-        self.graphs.append(newborn)
-        nbnewborn+=1
+        return nbnewborn
         
-    return nbnewborn
-        
-        
-    
-P1 = Population(10,0.5,2,0.1,0.1,0.2,[1,1,1])
+    def save_pop(self): # Save the current graph population in a textfile
+        mypath = 'popsave'
+        if not os.path.isdir(mypath):
+            os.makedirs(mypath)
+        for i in xrange(len(self.graphs)):
+            s = './popsave/g'+str(i)+'.txt'
+            fh=open(s,'wb')
+            nx.write_adjlist(self.graphs[i],fh)
+            fh.close()
+
+    def load_pop(self,nbofgraphs,mypath): # Load a graph population from a directory
+	mypath = 'popsave'
+        if os.path.isdir(mypath):
+	    self.graphs = []
+	    for i in xrange(nbofgraphs):
+		s = './popsave/g'+str(i)+'.txt'
+		fh=open(s,'r')
+		self.graphs.append(nx.read_adjlist(fh))
+		fh.close()
+		print i+1
+	else :
+	    print 'Save directory not found'
+
+    def show_best(self): # Plot the graph with the best fitness of our current population
+	list_fit = self.fitness_list()
+        nx.draw_spectral(self.graphs[list_fit.index(max(list_fit))])
+	print "printing graph number " + str(list_fit.index(max(list_fit))) + " "  
+        plt.show()
+	
+P1 = Population(10,0.5,4,0.1,0.1,0.2,[1,1,1])
 print P1.fitness(P1.graphs[0])
-
+P1.save_pop()
+P1.load_pop(4,'popsave')
+P1.show_best()
